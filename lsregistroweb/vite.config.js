f882f23703ab64cbbf492864ac654de4e34e0e3a
megadatogit <megadato82@gmail.com>
2025-08-07 15:39:08 -0600

    vite_base

--- lsregistroweb/vite.config.js
+++ lsregistroweb/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.BASE,
-  plugins: [react()],
-  resolve:{
-    alias:{
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE || '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
 });
