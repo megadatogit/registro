e0610e5b1372a11c92a5625d2ec17dfa6379c07c
megadatogit <megadato82@gmail.com>
2025-08-25 17:09:06 -0600

    tarjetas estados cuestionarios estilos dinamicos

--- lsinciosesionweb/src/components/Tarjetas/TarjetaCuestionarios/TarjetaCuestionario.jsx
+++ lsinciosesionweb/src/components/Tarjetas/TarjetaCuestionarios/TarjetaCuestionario.jsx
@@ -3,22 +3,69 @@ import styles from './tarjetaCuestionario.module.css';
 import TrjEstadoCuestionario from './TrjEstadoCuestionario';
 import TarjetaEvaluacion from '../TarjetaEvaluacion/TarjetaEvaluacion';
 
-const TarjetaCuestionario = () => {
+import completado from  './completado.svg'
+import proceso from  './proceso.svg'
+import inactivo from  './candado.svg'
+
+
+const estados = ["inactivo", "completado", "proceso"]
+
+  const perfilCuestionario = 
+  [
+    {
+    id: 1,
+    nombre: "Cuestionario de Satisfacción",
+    descripcion: "Evalúa la satisfacción del cliente con el servicio.",
+    estado: 1,
+    fecha: "01/01/2023",
+  },
+  {
+    id: 2,
+    nombre: "Cuestionario de Evaluación",
+    descripcion: "Evalúa el desempeño del empleado.",
+    estado: 0,
+    fecha: "01/01/2023"
+  },
+  {
+    id: 3,
+    nombre: "Cuestionario de Clima Laboral",
+    descripcion: "Evalúa el ambiente laboral en la empresa.",
+    estado: 2,
+    fecha: "01/01/2023"
+  }
+
+  ];
+
+
+  const srcIcon = {
+    completado,
+    proceso,
+    inactivo
+  };
+
+  const TarjetaCuestionario = () => {
+
+
   return (
     <div className={styles.cntTarjetaCuestionario}>
+
         <h3 className={styles.tituloCuestionario}>Cuestionarios</h3>
         
         <div className={styles.cntEstadosCuestionario}>
-            <TrjEstadoCuestionario />
-            <TrjEstadoCuestionario />
-            <TrjEstadoCuestionario />
-            <TrjEstadoCuestionario />
-            <TrjEstadoCuestionario />
-            
-            
+            {perfilCuestionario.map((cuestionario) => (
+                <TrjEstadoCuestionario 
+                    key={cuestionario.id}
+                    id={cuestionario.id}
+                    nombre={cuestionario.nombre}
+                    descripcion={cuestionario.descripcion}
+                    estado={estados[cuestionario.estado]}
+                    fecha={cuestionario.fecha}
+                    srcIcon={srcIcon[estados[cuestionario.estado]]}
+                    name={estados[cuestionario.estado]}
+
+                />
+            ))}
         </div>
-        
-        
     </div>
   )
 }
--- lsinciosesionweb/src/components/Tarjetas/TarjetaCuestionarios/TrjEstadoCuestionario.jsx
+++ lsinciosesionweb/src/components/Tarjetas/TarjetaCuestionarios/TrjEstadoCuestionario.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import styles from './trjEstadoCuestionario.module.css';
 import completado from './completado.svg'
 
-const TrjEstadoCuestionario = () => {
+const TrjEstadoCuestionario = ({ id, nombre, descripcion, estado, fecha, srcIcon }) => {
   return (
-    <div className={styles.cntTarjetaEstado}>
-
-        <div className={styles.cuestionarioDato}>
-            <p className={styles.tituloEstadoCuestionario}>Título de cuestionario</p>
-            <p className={styles.fecha}>Fecha de creación: 01/01/2023</p>
+    <div key={id} className={styles.cntTarjetaEstado}>
+        <div className={styles.cuestionarioDato} data-estado={estado}>
+            <p className={styles.tituloEstadoCuestionario}>{nombre}</p>
+            <p className={styles.fecha}>Fecha de creación: {fecha}</p>
+            <p className={styles.fecha}>{descripcion}</p>
         </div>
         <div className={styles.estado}>
-            <p className={styles.estadoTxt}>Completado</p>
-            <img className={styles.estadoImg} src={completado} alt="Estado" />
+            <p className={styles.estadoTxt}>{estado}</p>
+            <img className={styles.estadoImg} src={srcIcon} alt={estado} />
         </div>
-
     </div>
   )
 }
